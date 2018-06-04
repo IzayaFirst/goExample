e170380e80c0be5d4c9a7e729fc218fd63ee44fc
First Patchara <patcharaleelalumplert@gmail.com>
2018-06-04 12:03:39 +0700

    if else and switch case

--- helloworld.go
+++ helloworld.go
@@ -7,5 +7,19 @@ func main() {
 	var input float64
 	fmt.Scanf("%f", &input)
 	fmt.Println("Input Data is :", input)
+	switch input {
+	case 0:
+		fmt.Println("This is 0")
+	case 1:
+		fmt.Println("This is 1")
+	default:
+		fmt.Println("No case")
+	}
+	/* if input > 10 {
+		fmt.Println("More than 10")
+	} else {
+		fmt.Println("Less than 10 ")
+	} */
+
 	// fmt.Println("Hello world")
 }
