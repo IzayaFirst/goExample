0bd80a1b27e8352b5cacfe1fb23ce0e708547cbd
First Patchara <patcharaleelalumplert@gmail.com>
2018-06-04 11:47:53 +0700

    variable

--- helloworld.go
+++ helloworld.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	var data string = "Hello world"
+	data2 := "Hello world"
+	interger := 200
+	fmt.Println(data)
+	fmt.Println(data2)
+	fmt.Println(interger)
+	// fmt.Println("Hello world")
 }
