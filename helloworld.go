83a9b566c79e085c49366ed0d76ebc5e0cc43e91
First Patchara <patcharaleelalumplert@gmail.com>
2018-06-04 12:09:17 +0700

    add for loop

--- helloworld.go
+++ helloworld.go
@@ -3,18 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Input Data :")
-	var input float64
-	fmt.Scanf("%f", &input)
-	fmt.Println("Input Data is :", input)
-	switch input {
-	case 0:
-		fmt.Println("This is 0")
-	case 1:
-		fmt.Println("This is 1")
-	default:
-		fmt.Println("No case")
+	/*
+		fmt.Println("Input Data :")
+		var input float64
+		fmt.Scanf("%f", &input)
+		fmt.Println("Input Data is :", input)
+	*/
+	for i := 0; i <= 10; i++ {
+		if i%2 == 0 {
+			fmt.Println(i, "is even ")
+		} else {
+			fmt.Println(i, "is odd ")
+		}
 	}
+
+	/*
+		switch input {
+			case 0:
+				fmt.Println("This is 0")
+			case 1:
+				fmt.Println("This is 1")
+			default:
+				fmt.Println("No case")
+			} */
 	/* if input > 10 {
 		fmt.Println("More than 10")
 	} else {
